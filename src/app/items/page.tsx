cf4cd42f2de5293c9132f00e215dec438498c117
julianParrax <julianfranco.parra@gmail.com>
2023-09-20 10:47:54 -0300

    Added:02. Added page of teails for each product, getting the data using the id

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,49 +1,64 @@
 //esta es la ruta que obtiene la informacion de la query y busca el articulo
 //api: https://api.mercadolibre.com/sites/MLA/search?q=
 
+import Link from "next/link";
+
 //para el ejemplo
 //https://api.mercadolibre.com/sites/MLA/search?q=iphone&limit=4
-export default async function ItemsPage({searchParams}:{searchParams: {search:string}}){
+export default async function ItemsPage({
+  searchParams,
+}: {
+  searchParams: { search: string };
+}) {
   // Aquí, se realiza una solicitud HTTP a la API de MercadoLibre utilizando la función fetch. La URL se construye utilizando el valor search de searchParams. Luego, se analiza la respuesta JSON para obtener el campo results. La respuesta JSON se tipa explícitamente para asegurarse de que coincida con la estructura esperada.
-  const {results} = await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${searchParams.search}&limit=7
-  `).then(res => res.json() as Promise<{
-    results: {
-      id: string;
-      title: string;
-      thumbnail: string;
-      price: number;
-      currency_id: string;
-      seller_address: {
-        city: {
-          name: string,
-        };
-      };
-    }[]
-  }>)
+  const { results } =
+    await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${searchParams.search}&limit=4
+  `).then(
+      (res) =>
+        res.json() as Promise<{
+          results: {
+            id: string;
+            title: string;
+            thumbnail: string;
+            price: number;
+            currency_id: string;
+            seller_address: {
+              city: {
+                name: string;
+              };
+            };
+          }[];
+        }>
+    );
   //Imprime los resultados en la consola para depuración
-  console.log(results)
+  console.log(results);
   return (
     <section>
       <article className="grid gap-4">
         {/* Mapea los resultados y crea un elemento para cada "item". */}
-        {results.map((item)=>(
-          <div key={item.id} className="flex gap-4">
+        {results.map((item) => (
+          <Link href={`/items/${item.id}`} key={item.id} className="flex gap-4">
             <img src={item.thumbnail} alt={item.title} />
             <div>
-              {/* Muestra el precio formateado como moneda y el título del "item". */}
-              <p className="font-bold text-xl">{Number(item.price).toLocaleString('es-AR',{style:'currency', currency: item.currency_id})}</p>
+              {/* Muestra el precio formateado como moneda y el título del "item". 
+              Este P es de uso recurrento asique lo podria convertir en component*/}
+              <p className="font-bold text-xl">
+                {Number(item.price).toLocaleString("es-AR", {
+                  style: "currency",
+                  currency: item.currency_id,
+                })}
+              </p>
               <p>{item.title}</p>
             </div>
             {/* Esto realiza una validacion, en caso de que ese vlaor no se pueda leer*/}
-            <span className="ml-auto  text-sm capitalize opacity-50">{item.seller_address ? item.seller_address.city.name.toLowerCase() : ''}</span>
-          </div>
+            <span className="ml-auto  text-sm capitalize opacity-50">
+              {item.seller_address
+                ? item.seller_address.city.name.toLowerCase()
+                : ""}
+            </span>
+          </Link>
         ))}
       </article>
     </section>
   );
 }
-
-
- 
-
-
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,14 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body >
-        <header className='flex h-16 bg-yellow-300 px-4'>
+        <header className='flex h-16 bg-yellow-300 '>
             {/* cuando toco el formulario, va a redirigir  mi carpeta items, el contenido en el input search, y lo va agregar a los queries params (esto seria mas eficiente si se hiciera con el client side navigation de next pero asi es mas sencillo); en un challenge real seria mejor hacer que el formulario sea un client component, y despues uso el prevent default usando el router de  next.js pára hacer un soft transition*/}
-          <form action="/items" className='m-auto flex max-w-screen-xl flex-1 gap-4'>
+          <form action="/items" className='px-4 flex max-w-screen-lg flex-1 m-auto gap-4'>
             <input type="text" name="search" className='h-8 flex-1 px-2 text-white'/>
             <button className='bg-gray-200 px-2 py-1 text-slate-700'>Buscar</button>
           </form>
         </header>
-        <main className='max-w-screen-xl p-4'>{children}</main>
+        <main className='max-w-screen-lg p-4 m-auto'>{children}</main>
       </body>
     </html>
   )
